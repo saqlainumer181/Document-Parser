c3f31e1acadcdee7645ad3e5f9d4dbe72c5a74b3
saqlain-umer <saqlain.umer@cognimindai.com>
2024-07-11 17:06:30 +0500

    Update: added code to process multiple pdf

--- app.py
+++ app.py
@@ -1,54 +1,40 @@
-
-from utils import *
 import streamlit as st
 from dotenv import load_dotenv
-from groq import Groq
-import os
 
+from utils import get_conversation_chain, get_pdf_chunks, get_vector_store
 
 
 def main():
     load_dotenv()
 
-    st.set_page_config(page_title="PDF Parser",
-                       page_icon=":books:")
-    
+    st.set_page_config(page_title="PDF Parser", page_icon=":books:")
+
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
-    
+
     st.header("PDF Chatbot :books:")
     user_question = st.text_input("Ask a question about your documents:")
     with st.sidebar:
         st.subheader("Your documents")
         uploaded_files = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True
+        )
+
         if st.button("Process"):
             with st.spinner("Processing"):
-
                 if uploaded_files:
                     all_elements = []
                     for file in uploaded_files:
                         elements = get_pdf_chunks(filename=file)
+                        all_elements.append(elements)
 
-
-                text_chunks = get_pdf_chunks(pdf_path)
+                text_chunks = all_elements
 
                 vector_store = get_vector_store(text_chunks)
 
                 st.session_state.conversation = get_conversation_chain(vector_store)
-
-
-
-    
-
-
-
-    
-
-
 
 
 if __name__ == "__main__":
